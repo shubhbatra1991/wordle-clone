f20bbb0f1b149f48243b361575c1cae19265e307
shubhbatra1991 <godwithsam@gmail.com>
2023-03-24 16:11:08 +0000

    Second commit files.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Wordle from "./components/Wordle";
 
 function App() {
+  const [solution, setSolution] = useState(null);
+
+  useEffect(()=> {
+    fetch('http://localhost:3001/solutions')
+      .then(res => res.json())
+      .then(json =>{
+        //random int between 0 and 14
+        const randomSolution = json[Math.floor(Math.random()*json.length)]
+        setSolution(randomSolution.word);
+      })
+  },[setSolution])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Wordle Clone</h2>
+      {solution && <Wordle solution={solution} />}
     </div>
   );
 }
